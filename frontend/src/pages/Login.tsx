8d65ce9d22604fc02bc94a321b51d208f27fd68f
Ricardo Sousa <ricardofegosousa@gmail.com>
2024-11-05 20:51:01 +0000

    changed login screen

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import Sidebar from "../components/Sidebar/Sidebar.tsx";
+import styles from "./Login.module.css";
+import logo from "../assets/logo.jpg";
+import google_icon from "../assets/google_icon.png";
 
 const Login: React.FC = () => {
     return (
         <div className="app-container">
-            <Sidebar
-                userName="Salif Faustino"
-            />
-            <div className="main-content">
-                <h1>Welcome to FitEverywhere</h1>
+            <div className={styles.sideInfo}>
+                <div className={styles.headerDiv}>
+                    <h1>FitEverywhere</h1>
+                    <div className="logoContainer">
+                        <img src={logo} alt="Logo" className={styles.logo} />
+                    </div>
+                </div>
+                <p className={styles.infoText}>With FitEverywhere, maintaining a fitness routine becomes a rewarding part of the travel experience, making it easy to stay active and healthy—anytime, anywhere.</p>
+            </div>
+            <div className={styles.loginbox}>
+                <h1>Login</h1>
+                    <button>
+                        <div className={styles.buttonContainer}>
+                            Login with Google
+                            <div className="googleIconContainer">
+                                <img src={google_icon} alt="google" className={styles.googleIcon} />
+                            </div>
+                        </div>
+                    </button>
             </div>
         </div>
     );
