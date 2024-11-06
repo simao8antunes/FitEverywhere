34b7b74f1d1a46464d32ddbb00cb22100f5e57da
simao8antunes <simao8antunes@gmail.com>
2024-11-06 23:25:42 +0000

    login page with google auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,42 +2,12 @@
 import './App.css';
 //import Sidebar from './components/Sidebar/Sidebar';
 import React from 'react';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
-  /*const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userName, setUserName] = useState<string | null>(null);*/
-
-/*  useEffect(() => {
-    // Fetch user data to check if the user is authenticated
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/auth/login/success', {
-          credentials: 'include',
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setIsAuthenticated(true);
-          setUserName(data.user.username);  // Assuming 'name' is the attribute containing the user’s name
-        } else {
-          setIsAuthenticated(false);
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-        setIsAuthenticated(false);
-      }
-    };
-
-    fetchUserData();
-  }, []);
-
-  const handleLogin = () => {
-    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
-  };*/
-
   return (
     <div className="app-container">
       <BrowserRouter>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import styles from "./Login.module.css";
-import logo from "../assets/logo.jpg";
+import React, { useEffect, useState } from 'react';
 import google_icon from "../assets/google_icon.png";
+import logo from "../assets/logo.jpg";
+import styles from "./Login.module.css";
 
 const Login: React.FC = () => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [userName, setUserName] = useState<string | null>(null);
+
+    useEffect(() => {
+        // Fetch user data to check if the user is authenticated
+        const fetchUserData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/auth/login/success', {
+            credentials: 'include',
+            });
+            if (response.ok) {
+            const data = await response.json();
+            setIsAuthenticated(true);
+            setUserName(data.user.username);  // Assuming 'name' is the attribute containing the user’s name
+            } else {
+            setIsAuthenticated(false);
+            }
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+            setIsAuthenticated(false);
+        }
+        };
+
+    fetchUserData();
+    }, []);
+
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  };
+
+
     return (
         <div className="app-container">
             <div className={styles.sideInfo}>
@@ -18,7 +50,7 @@ const Login: React.FC = () => {
             <div className={styles.loginbox}>
                 <h1>Login</h1>
                     <button>
-                        <div className={styles.buttonContainer}>
+                        <div onClick={handleLogin} className={styles.buttonContainer}>
                             Login with Google
                             <div className="googleIconContainer">
                                 <img src={google_icon} alt="google" className={styles.googleIcon} />
