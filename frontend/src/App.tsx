fc90f6568c3f253a4cb1f5f17f5e35f37d5bbcbd
Salif Faustino <67168239+SalifNTC@users.noreply.github.com>
2024-11-12 01:43:32 -0100

    feat(login): Redesign Google login button for improved style and branding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import "./index.css";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import SelectRole from "./pages/SelectRole";
