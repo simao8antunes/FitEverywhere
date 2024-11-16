689fd3f07387d039f696fbc9b7c74b43eca4a450
simao8antunes <simao8antunes@gmail.com>
2024-11-16 11:20:15 +0000

    Controller Update

--- backend/src/main/java/pt/fe/up/fiteverywhere/backend/controller/AuthController.java
+++ backend/src/main/java/pt/fe/up/fiteverywhere/backend/controller/AuthController.java
@@ -14,18 +14,13 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import pt.fe.up.fiteverywhere.backend.entity.User;
 import pt.fe.up.fiteverywhere.backend.service.UserService;
 
@@ -35,69 +30,6 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
-
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User user) {
-        if (userService.isUserExists(user.getUsername(), user.getEmail())) {
-            Map<String, String> response = new HashMap<>();
-            response.put("error", "User already exists");
-            return ResponseEntity.badRequest().body(response);
-        }
-        userService.registerUser(user.getUsername(), user.getEmail(), user.getPassword());
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "User registered successfully");
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody User user) {
-        User loggedInUser = userService.loginUser(user.getUsername(), user.getPassword());
-        if (loggedInUser != null) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("message", "Login successful");
-            response.put("user", loggedInUser);  // Or just return a subset like ID, username, roles, etc.
-            return ResponseEntity.ok(response);
-        } else {
-            Map<String, String> response = new HashMap<>();
-            response.put("error", "Invalid credentials");
-            return ResponseEntity.badRequest().body(response);
-        }
-    }
-
-    @GetMapping("/logout")
-    public ResponseEntity<Map<String, String>> logoutUser(HttpServletRequest request, HttpServletResponse response) {
-        new SecurityContextLogoutHandler().logout(request, response, null);
-        Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("message", "Logout successful");
-        return ResponseEntity.ok(responseBody);
-    }
-
-
-    @GetMapping("/")
-    public ResponseEntity<Map<String, String>> root() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Welcome to the FitEverywhere API");
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/login/success")
-    public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal OAuth2User principal) {
-        if (principal == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
-        }
-
-        String email = principal.getAttribute("email");
-        String name = principal.getAttribute("name");
-        System.out.println("Authenticated user: " + name);  // Add debug log to check the value
-
-        User user = userService.findOrRegisterOAuthUser(name, email);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Login successful");
-        response.put("user", user); // Return user info
-        return ResponseEntity.ok(response);
-    }
-
 
     @PutMapping("/role")
     public ResponseEntity<Map<String, String>> updateUserRole(
@@ -150,7 +82,6 @@ public class AuthController {
                     .body(Map.of("error", "Failed to fetch calendar events", "message", e.getMessage()));
         }
     }
-
 
 
     @GetMapping("/gyms/nearby")
