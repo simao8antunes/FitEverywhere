95f03af7db5d0a220beb43ae30c60d47184deaef
Nuno Fernandes <nuno.o.fernandes@outlook.pt>
2024-11-20 16:59:07 +0000

    fixed provider bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Layout from "./components/Layout";
 import SelectRole from "./pages/SelectRole";
-import AuthProvider from "./providers/AuthProvider";
 import Profile from "./pages/Profile.tsx";
 import GymProfile from "./pages/GymProfile.tsx";
 import { useAuth } from "./hooks/useAuth.ts";
@@ -13,32 +12,28 @@ import { useAuth } from "./hooks/useAuth.ts";
 const App: React.FC = () => {
   const { user } = useAuth();
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="select-role" element={<SelectRole />} />
-          {user?.role === "gym" && (
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="profile" element={<GymProfile />} />
-            </Route>
-          )}
-          {user?.role === "client" && (
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
-          )}
-          {user?.role === "pt" && (
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
-          )}
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="select-role" element={<SelectRole />} />
+      {user?.role === "gym" && (
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="profile" element={<GymProfile />} />
+        </Route>
+      )}
+      {user?.role === "client" && (
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      )}
+      {user?.role === "pt" && (
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      )}
+    </Routes>
   );
 };
 
