3896177f56c2315297a3a7e2db97e486c2dff772
Nuno Fernandes <nuno.o.fernandes@outlook.pt>
2024-11-20 16:49:13 +0000

    added route through role

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,35 @@ import Layout from "./components/Layout";
 import SelectRole from "./pages/SelectRole";
 import AuthProvider from "./providers/AuthProvider";
 import Profile from "./pages/Profile.tsx";
+import GymProfile from "./pages/GymProfile.tsx";
+import { useAuth } from "./hooks/useAuth.ts";
 
 const App: React.FC = () => {
+  const { user } = useAuth();
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
           <Route path="login" element={<Login />} />
           <Route path="select-role" element={<SelectRole />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
+          {user?.role === "gym" && (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="profile" element={<GymProfile />} />
+            </Route>
+          )}
+          {user?.role === "client" && (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+          )}
+          {user?.role === "pt" && (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+          )}
         </Routes>
       </AuthProvider>
     </BrowserRouter>
