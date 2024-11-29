7b4d155f83cc1635374266cd440afe4027a53c60
Nuno Fernandes <105490848+NOF03@users.noreply.github.com>
2024-11-29 04:53:59 +0000

    Fixed deployment with database for prod and dev (#113)
    * Fixed deployment with database for prod and dev
    
    * fixed database issue
    
    * fixed ci issue
    
    * fixed ci envfile

--- backend/src/main/java/pt/fe/up/fiteverywhere/backend/SecurityConfig.java
+++ backend/src/main/java/pt/fe/up/fiteverywhere/backend/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList(environment.getProperty("server.name")));
+        config.setAllowedOrigins(Collections.singletonList(environment.getProperty("SERVER_NAME")));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         source.registerCorsConfiguration("/**", config);
