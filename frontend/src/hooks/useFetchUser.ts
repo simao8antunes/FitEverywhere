5d094265cb3bf4078599ce15498b8a7f686c1aa7
Nuno Fernandes <nuno.o.fernandes@outlook.pt>
2024-12-15 13:09:06 +0000

    Fixed tests

--- frontend/src/hooks/useFetchUser.ts
+++ frontend/src/hooks/useFetchUser.ts
@@ -46,6 +46,7 @@ export function useFetchUser(): UseFetchUserResult {
       setIsAuthenticated(false);
       setUser(null);
       setError("Failed to authenticate");
+      navigate("/login");
     }
   };
 
